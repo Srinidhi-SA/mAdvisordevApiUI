9b2560a0c512b6e6b200a3c5f2c23893339af47f
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2020-12-22 16:49:00 +0530

    test pushls

--- api_image_generate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash 
-cd API_DOCKER
-docker build -t api .
-docker tag api:latest 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_dev_api:latest
-$(aws ecr get-login --region us-west-1 --no-include-email) 
-docker push 115502491259.dkr.ecr.us-west-1.amazonaws.com/madvisor_dev_api:latest
-cd ..
