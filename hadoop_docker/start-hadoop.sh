77fc001205f6507c88199a448362c66dae116236
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2020-12-27 20:23:19 +0530

    test push

--- hadoop_docker/start-hadoop.sh
+++ hadoop_docker/start-hadoop.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 /etc/init.d/ssh start
-echo "Run namenode"
-$HADOOP_HOME/bin/hdfs namenode -format
-echo "Run startnode"
+#$HADOOP_HOME/bin/hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
-echo "Run StartManager"
 $HADOOP_HOME/sbin/start-yarn.sh
-echo "Run ResourceManager"
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
-
 #Permissions for marlabs user
-hdfs dfs -mkdir /user/marlabs && hdfs dfs -chown marlabs:supergroup /user/marlabs
-hdfs dfs -chown marlabs:supergroup /tmp
+#hdfs dfs -mkdir /user/marlabs && hdfs dfs -chown marlabs:supergroup /user/marlabs
+#hdfs dfs -chown marlabs:supergroup /tmp
 
  
 tail -f /dev/null 
