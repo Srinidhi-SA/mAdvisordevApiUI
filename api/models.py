74a7f44e32cf1959617a9381161f601494f6a5c7
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2021-01-22 00:29:31 +0530

    test file changes

--- api/models.py
+++ api/models.py
@@ -396,8 +396,8 @@ class Dataset(models.Model):
     def copy_file_to_hdfs(self):
         file_size = os.stat(self.input_file.path).st_size
         try:
-            if file_size > 128000000:
-                hadoop.hadoop_put(self.input_file.path, self.get_hdfs_relative_path())
+            #if file_size > 128000000:
+            hadoop.hadoop_put(self.input_file.path, self.get_hdfs_relative_path())
         except:
             raise Exception("Failed to copy file to HDFS.")
 
--- hadoop_docker/start-hadoop.sh
+++ hadoop_docker/start-hadoop.sh
@@ -4,6 +4,7 @@
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
 #Permissions for marlabs user
 #hdfs dfs -mkdir /user/marlabs && hdfs dfs -chown marlabs:supergroup /user/marlabs
 #hdfs dfs -chown marlabs:supergroup /tmp
