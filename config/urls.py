60ecb88db4a0de0e17326283ff79db30be212f55
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-02-19 14:21:33 +0530

    Test Push

--- config/urls.py
+++ config/urls.py
@@ -52,5 +52,7 @@ urlpatterns = [
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-urlpatterns += url(r'^', home_view)
+#urlpatterns += url(r'^', home_view)
+urlpatterns += [url(r'^', home_view)]
+
 
