5f3f425bf61a92ad26f269089045d693b58bf354
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-06-09 11:11:02 +0530

    API Patch

--- Worker.sh
+++ Worker.sh
@@ -1,4 +1,4 @@
-rsync -r * API_DOCKER/code/mAdvisor-api/ --exclude API_DOCKER --exclude copyApiFolder.sh --exclude buildspec.yml --exclude hadoop_docker --exclude NGINX_DOCKER --exclude copyHadoopImage.sh --exclude requirements --exclude WORKER_DOCKER --exclude Worker.sh
+rsync -r * WORKER_DOCKER/code/mAdvisor-api/ --exclude API_DOCKER --exclude copyApiFolder.sh --exclude buildspec.yml --exclude hadoop_docker --exclude NGINX_DOCKER --exclude copyHadoopImage.sh --exclude requirements --exclude WORKER_DOCKER --exclude Worker.sh
 cp -r requirements WORKER_DOCKER/requirements/
 cd WORKER_DOCKER
 docker build -t $REPOSITORY_URI_WORKER:latest .
