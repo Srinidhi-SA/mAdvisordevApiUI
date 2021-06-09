c3d0a58bf4636ae0b780c2803dd3a9f566ad30d5
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-06-09 21:29:16 +0530

    Test Push

--- WORKER_DOCKER/startup.sh
+++ WORKER_DOCKER/startup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sleep 3s
-celery worker -A config.celery --beat --scheduler 'django_celery_beat.schedulers:DatabaseScheduler' -l info
+celery worker -A config.celery --beat --scheduler 'django_celery_beat.schedulers:DatabaseScheduler' -l info -Q luke &
+sh lock.sh &
+tail -f /dev/null
